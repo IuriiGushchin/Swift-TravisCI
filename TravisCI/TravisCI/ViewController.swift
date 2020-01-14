760b8cb4f7594199e11c049908890c131afb70f9
iuriigushchin <yurii.gushchin@ledgerleopard.com>
2020-01-14 14:51:29 +0300

    add simple project for TravisCI test

---
+++ TravisCI/TravisCI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravisCI
+//
+//  Created by Юрий Гущин on 14.01.2020.
+//  Copyright © 2020 LedgerLeopard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
